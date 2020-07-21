c516c936466fe79edea3bed2dec71c9fefe7f1a3
sshabelnik <s.shabelnik01@gmail.com>
2020-07-21 16:37:03 +0600

    empry project

---
+++ covid-19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  covid-19
+//
+//  Created by Сергей Шабельник on 21.07.2020.
+//  Copyright © 2020 Сергей Шабельник. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
